24fe0331486c23d4e24323e9b0eb651e0550ba2b
wesselvandenijssel <wessel069@gmail.com>
2023-11-28 22:06:21 +0100

    disabled image scaling

--- functions.php
+++ functions.php
@@ -30,3 +30,5 @@ add_action('after_setup_theme', function () {
     // Translations can be filed in the /languages/ directory.
     load_theme_textdomain('flynt', get_template_directory() . '/languages');
 });
+
+add_filter('big_image_size_threshold', '__return_false');
