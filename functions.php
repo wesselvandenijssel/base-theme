179826e084600760251198440a4299f4578d58c9
wesselvandenijssel <wessel069@gmail.com>
2023-11-28 22:13:35 +0100

    removed quality

--- functions.php
+++ functions.php
@@ -31,4 +31,9 @@ add_action('after_setup_theme', function () {
     load_theme_textdomain('flynt', get_template_directory() . '/languages');
 });
 
-add_filter('big_image_size_threshold', '__return_false');
+add_filter('jpeg_quality', function ($arg) {
+    return 100;
+});
+add_filter('wp_editor_set_quality', function ($arg) {
+    return 100;
+});
