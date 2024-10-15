4bad4d2e3a6d6f3170597ca29fa320dd627bbe33
wesselvandenijssel <wessel069@gmail.com>
2024-10-15 21:49:14 +0200

    added code block

---
+++ Components/BlockCode/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Flynt\Components\BlockCode;
+
+use Flynt\FieldVariables;
+
+function getACFLayout() {
+    return [
+        'name' => 'blockCode',
+        'label' => __('Block: Code', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => __('Text', 'flynt'),
+                'name' => 'contentHtml',
+                'type' => 'wysiwyg',
+                'delay' => 0,
+                'media_upload' => 0,
+                'required' => 1,
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme(),
+                    FieldVariables\getSize(),
+                    FieldVariables\getAlignment(),
+                    FieldVariables\getTextAlignment()
+                ]
+            ]
+        ]
+    ];
+}
--- inc/fieldGroups/pageComponents.php
+++ inc/fieldGroups/pageComponents.php
@@ -24,6 +24,7 @@ add_action('Flynt/afterRegisterComponents', function () {
                     Components\BlockSpacer\getACFLayout(),
                     Components\BlockVideoOembed\getACFLayout(),
                     Components\BlockWysiwyg\getACFLayout(),
+                    Components\BlockCode\getACFLayout(),
                     Components\GridImageText\getACFLayout(),
                     Components\GridPostsLatest\getACFLayout(),
                     Components\ListComponents\getACFLayout(),
